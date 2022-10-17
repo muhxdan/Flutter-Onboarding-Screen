82c498c3906125ae0b5e5872309ce2249d878658
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2022-10-17 12:12:35 +0330

    clean up the code

--- lib/onboarding_contents.dart
+++ lib/onboarding_contents.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
-
 class OnboardingContents {
   final String title;
   final String image;
   final String desc;
 
-  OnboardingContents(
-      {required this.title, required this.image, required this.desc});
+  OnboardingContents({
+    required this.title,
+    required this.image,
+    required this.desc,
+  });
 }
 
 List<OnboardingContents> contents = [
